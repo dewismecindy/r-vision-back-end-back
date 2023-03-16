7cea1715a5fa3833de94c1d61d811db572239001
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-03-16 20:03:11 +0100

    recupération des params

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const nameTab = ["Iris", "Olympe", "Rachel"];
 
 app.get("/", (req, res) => {
-  console.log("Je suis passé dans ma route");
+  /*   console.log("Je suis passé dans ma route"); */
+  console.log(req.query);
   res.json("Championne ! Tu es sur la route /");
 });
 
 app.get("/get-name", (req, res) => {
   res.status(200).json(nameTab);
+});
+
+app.post("/create/names", (req, res) => {
+  console.log(req.body);
+  nameTab.push(req.body.country);
+
+  res.status(200).json("Country is create");
+});
+app.get("/animals/:age", (req, res) => {
+  console.log(req.params.age);
 });
 
 app.all("*", (req, res) => {
